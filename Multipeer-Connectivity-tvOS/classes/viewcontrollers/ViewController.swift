4c4183c2570e781abcc19ab3414f9d2fe9131631
Kohei Tabata <nerd0geek1@gmail.com>
2016-10-17 22:53:27 +0900

    integrate UI components with multipeer networking classes

--- Multipeer-Connectivity-tvOS/classes/viewcontrollers/ViewController.swift
+++ Multipeer-Connectivity-tvOS/classes/viewcontrollers/ViewController.swift
@@ -10,7 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var buttonsView: UIView!
+
+
+    //MARK: - lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupMultiPeerNetworking()
+    }
+
+    //MARK: - private
+
+    private func setupMultiPeerNetworking() {
+        MultipeerSession.sharedInstance.didEstablishConnection = {
+            DispatchQueue.main.async {
+                self.backgroundView.isHidden = true
+                self.buttonsView.isHidden    = true
+            }
+        }
+        MultipeerSession.sharedInstance.didReceiveResource = { resource in
+            NSLog("resource:\(resource.text)")
+        }
+
+        MultipeerBrowser.sharedInstance.setup(with: MultipeerSession.sharedInstance)
+        MultipeerAdvertiser.sharedInstance.setup(with: MultipeerSession.sharedInstance)
+    }
+
+    //MARK: - IBAction
+
+    @IBAction
+    private func tapMakeGroupButton() {
+        MultipeerBrowser.sharedInstance.startBrowsing()
+    }
+
+    @IBAction
+    private func tapJoinGroupButton() {
+        MultipeerAdvertiser.sharedInstance.startAdvertising()
     }
 }
